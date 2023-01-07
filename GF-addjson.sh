52f70a95a0546860f75ac93664c5854caed8dbbb
foorw1nner <108235281+foorw1nner@users.noreply.github.com>
2023-01-07 03:13:53 -0400

    Update GF-addjson.sh

--- GF-addjson.sh
+++ GF-addjson.sh
@@ -2,11 +2,11 @@ echo "{
      \"flags\": \"$1\", 
      \"patterns\": [
 
-" >> /root/.gf/$3.json
-cat $2 | sed s'/^/"/' | sed s'/$/",/' >> /root/.gf/$3.json
-echo "\" \"" >> /root/.gf/$3.json
+" >> /root/.gf/$3
+cat $2 | sed s'/^/"/' | sed s'/$/",/' >> /root/.gf/$3
+echo "\" \"" >> /root/.gf/$3
 echo "
 
 ]
 }
-" >> /root/.gf/$3.json
+" >> /root/.gf/$3
